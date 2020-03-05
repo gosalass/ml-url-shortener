8156f5b6dce8e826049c6ced4812b0cc69cb2b93
Gonzalo Salas <GONZALOSALAS.GLOBANT@latam.com>
2020-03-05 05:49:19 -0300

    Control redirection when shorten was deleted

--- src/main/java/com/ml/shortener/infraestructure/entrypoints/controller/UrlRedirectController.java
+++ src/main/java/com/ml/shortener/infraestructure/entrypoints/controller/UrlRedirectController.java
@@ -3,8 +3,11 @@ package com.ml.shortener.infraestructure.entrypoints.controller;
 import com.ml.shortener.domain.entities.response.UrlResponse;
 import com.ml.shortener.domain.usescases.ShortenUseCase;
 import java.net.URI;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,13 +26,18 @@ public class UrlRedirectController {
   }
 
   @GetMapping(value = "/{shortenUrl}")
-  public ResponseEntity<Void> redirect(@PathVariable(value = "shortenUrl") final String shortenUrl) {
+  public ResponseEntity<Void> redirect(@PathVariable("shortenUrl") String shortenUrl,
+       @CookieValue(value = "urlShorten", defaultValue = "EMPTY") String urlCookie, HttpServletResponse response) {
 
-    final UrlResponse urlResponse = this.shortenUseCase.retrieveOriginalUrlByShortenPath(shortenUrl);
+    if("EMPTY".equalsIgnoreCase(urlCookie)) {
+      Cookie cookie = new Cookie("urlShorten", shortenUrl);
+      response.addCookie(cookie);
+    }
 
+    UrlResponse urlResponse = this.shortenUseCase.retrieveOriginalUrlByShortenPath(shortenUrl);
     return
         ResponseEntity
-          .status(HttpStatus.MOVED_PERMANENTLY)
+          .status(HttpStatus.TEMPORARY_REDIRECT)
             .location(URI.create(urlResponse.getRef()))
               .build();
   }
